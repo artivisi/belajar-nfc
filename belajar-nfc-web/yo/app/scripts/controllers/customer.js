0ae68a96b177347eb20012f6ad1bfc26247276af
sholihin <mohamad.sholihin.it@gmail.com>
2015-04-13 11:26:31 +0700

    integrasi ui customer tahap 1

--- belajar-nfc-web/yo/app/scripts/controllers/customer.js
+++ belajar-nfc-web/yo/app/scripts/controllers/customer.js
@@ -1,13 +1,24 @@
 'use strict';
 
 angular.module('belajarNfcappApp')
-  .controller('CustomerCtrl', function ($scope) {
-
+  .controller('CustomerCtrl', function ($scope, $http, customerservice) {
+  	$scope.customers = {};
     $scope.opentanggalLahir = function($event) {
         $event.preventDefault();
-        $event.stopPropagation();
-        
+        $event.stopPropagation();  
         $scope.openedtanggalLahir = true;
     };
-    
+
+ 	customerservice.query().success(function(data){
+ 		$scope.customers = data;
+ 	});
+
+ 	$scope.deleteCustomer = function(x){
+ 		customerservice.remove(x).success(function(){
+			alert("data berhasil di hapus");
+ 		}).error(function(){
+			alert("data gagal di hapus");
+ 		})
+ 	}
+
   });
