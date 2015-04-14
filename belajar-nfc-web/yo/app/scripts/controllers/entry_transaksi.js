ae9389a6fb25b435e4ff4eedc4adff756520fbff
Lukman Hakim <lukmanh1995@gmail.com>
2015-04-14 14:15:16 +0700

    implementasi entri transaksi

--- belajar-nfc-web/yo/app/scripts/controllers/entry_transaksi.js
+++ belajar-nfc-web/yo/app/scripts/controllers/entry_transaksi.js
@@ -1,10 +1,66 @@
 'use strict';
 
 angular.module('belajarNfcappApp')
-  .controller('EntryTransaksiCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('EntryTransaksiCtrl', function ($scope, entrytransaksiservice, customerservice) {
+    $scope.transactions = {};
+    $scope.customers = {};
+    $scope.currentEntryTransaction = {};
+
+    $scope.reloadCustomer = function (){
+    	customerservice.query().success(function(data){
+    		$scope.customers = data.content;
+    	});
+    }
+
+    $scope.reloadTransaction = function(){
+	    entrytransaksiservice.query().success(function(data){
+	 		$scope.transactions = data;
+	 	});	
+    }
+    
+    $scope.openTanggal = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();  
+        $scope.tanggalIsOpened = true;
+    }
+
+    $scope.edit = function (selectedTransaction) {
+        $scope.currentEntryTransaction = selectedTransaction;
+        $scope.original = angular.copy($scope.currentEntryTransaction);
+    }
+
+    $scope.clearForm = function(){
+    	$scope.currentEntryTransaction = {};
+    }
+
+    $scope.delete = function(x){
+	    bootbox.confirm('Apakah anda yakin akan menghapus ?', function(result) {
+	        if(result){
+	            entrytransaksiservice.remove(x.id)
+            		.success(function(){
+            			bootbox.alert('Data berhasil dihapus');
+            		})
+            		.error(function() {
+            			bootbox.alert('Data gagal dihapus');
+            		});
+        		$scope.reloadTransaction();
+	        }
+	    }); 
+	};
+
+	$scope.save = function(x){
+		$scope.currentEntryTransaction.tanggal = new Date();
+		entrytransaksiservice.save(x)
+			.success(function() {
+				bootbox.alert('Data berhasil disimpan');
+			})
+			.error(function (){
+				bootbox.alert('Data gagal disimpan');
+			})
+			$scope.reloadTransaction();
+			$scope.clearForm();
+	}    
+    $scope.reloadCustomer();
+    $scope.reloadTransaction();
+
   });
--- belajar-nfc-web/yo/app/scripts/services/entrytransaksiservice.js
+++ belajar-nfc-web/yo/app/scripts/services/entrytransaksiservice.js
@@ -1,16 +1,25 @@
 'use strict';
 
 angular.module('belajarNfcappApp')
-  .factory('entrytransaksiservice', function () {
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
-
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
+  .factory('entrytransaksiservice', function ($http) {
+    return{
+      query : function(){
+          return $http.get('http://localhost:10000/api/entriTransaksi');
+      },
+      findOne : function(id){
+          return $http.get('http://localhost:10000/api/entriTransaksi/' + id);
+      },
+      save : function (obj){
+          if(obj.id == null){
+              return $http.post('http://localhost:10000/api/entriTransaksi/', obj);
+          } else {
+              return $http.put('http://localhost:10000/api/entriTransaksi/' + obj.id, obj);
+          }
+      },
+      remove : function (id){
+          if(id != null){
+              return $http.delete('http://localhost:10000/api/entriTransaksi/' + id);
+        }
       }
     };
   });
